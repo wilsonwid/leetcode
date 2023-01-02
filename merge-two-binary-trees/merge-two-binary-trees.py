9b78a88085b7feade99957163e898bdaec2cddc8
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2023-01-02 11:13:46 +0700

    Added merge-two-binary-trees

---
+++ merge-two-binary-trees/merge-two-binary-trees.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1 and not root2:
+            return
+        elif not root1:
+            return root2
+        elif not root2:
+            return root1
+        else:
+            root1.val += root2.val
+            root1.left = self.mergeTrees(root1.left, root2.left)
+            root1.right = self.mergeTrees(root1.right, root2.right)
+            return root1
